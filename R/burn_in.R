2e3f0b2a4653e3c8e21afe41ffc5a9dc736c7a45
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-08-02 16:27:42 +1000

    Add documentation to burn_in()

--- R/burn_in.R
+++ R/burn_in.R
@@ -1,3 +1,10 @@
+#' Burn in streams to a digital elevation model
+#' @description Burning-in streams (also called 'drainage reinforcement') ensures flow direction and accumulation grids based on the digital elevation model will correctly identify the stream path.
+#' @param dem A RasterLayer object containing the digital elevation model.
+#' @param stream A RasterLayer object containing a rasterised stream. The stream pixels should have a value of 1 and non-stream pixels should have a value of 0.
+#' @param burn The magnitude of the drainage reinforcement (in elevation units). Defaults to \code{10}.
+#' @return A RasterLayer containing the burned digital elevation model.
+#' @export 
 burn_in <- function(dem, stream, burn = 10){
   
   # Input checking
