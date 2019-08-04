2fe540d1a9fe58fe139a8f366ece62f29adae306
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-08-04 18:19:50 +1000

    Allow inputs to be file paths

--- R/burn_in.R
+++ R/burn_in.R
@@ -1,12 +1,16 @@
 #' Burn in streams to a digital elevation model
 #' @description Burning-in streams (also called 'drainage reinforcement') ensures flow direction and accumulation grids based on the digital elevation model will correctly identify the stream path.
-#' @param dem A RasterLayer object containing the digital elevation model.
-#' @param stream A RasterLayer object containing a rasterised stream. The stream pixels should have a value of 1 and non-stream pixels should have a value of 0.
+#' @param dem A RasterLayer object containing the digital elevation model. This can also be the file path to a raster. 
+#' @param stream A RasterLayer object containing a rasterised stream. The stream pixels should have a value of 1 and non-stream pixels should have a value of 0. This can also be the file path to a raster. 
 #' @param burn The magnitude of the drainage reinforcement (in elevation units). Defaults to \code{10}.
 #' @param out An optional string giving the basename of the raster to be written to file. If missing, then no file will be written.
 #' @return A RasterLayer containing the burned digital elevation model.
 #' @export 
 burn_in <- function(dem, stream, burn = 10, out){
+  
+  # If dem and stream are file paths, then import them as rasters
+  if(is.character(dem)) dem <- raster(dem)
+  if(is.character(stream)) stream <- raster(stream)
   
   # Input checking
   check_dem <- is_raster_layer(dem)
