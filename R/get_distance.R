d422f93649ae574d7dd60745b3620face91ad7ad
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-08-05 14:59:28 +1000

    Placeholder for get_distance

---
+++ R/get_distance.R
@@ -0,0 +1,14 @@
+#' Compute Euclidean distance to a survey site or stream line within a watershed
+#' @description This function is needed to compute Euclidean distance from a feature of interest in a watershed raster. 
+#' @param background A raster of the watershed outline. This can also be a file path to an existing raster.
+#' @param target A raster of the survey site or stream lines. This can also be a file path to an existing raster.
+#' @param out_file File path for the result to be written.
+#' @return Nothing. A file with the name and location \code{out_file} will be created. 
+get_distance <- function(background, target, out_file){
+  
+  
+  
+  # Return nothing
+  invisible()
+  
+}
