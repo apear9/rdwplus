2927028bd0e7d7c46742c3a32c991431541cce3c
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-08-08 08:08:40 +1000

    Argument no longer optional

--- R/burn_in.R
+++ R/burn_in.R
@@ -3,7 +3,7 @@
 #' @param dem A RasterLayer object containing the digital elevation model. This can also be the file path to a raster. 
 #' @param stream A RasterLayer object containing a rasterised stream. The stream pixels should have a value of 1 and non-stream pixels should have a value of 0. This can also be the file path to a raster. 
 #' @param burn The magnitude of the drainage reinforcement (in elevation units). Defaults to \code{10}.
-#' @param out An optional string giving the basename of the raster to be written to file. If missing, then no file will be written.
+#' @param out An string giving the basename of the raster to be written to file. If missing, then no file will be written.
 #' @return A RasterLayer containing the burned digital elevation model.
 #' @export 
 burn_in <- function(dem, stream, burn = 10, out){
