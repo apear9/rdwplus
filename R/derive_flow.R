e9c44a70bb77422728494bbff46b890f63dedd42
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-11 01:10:05 +1000

    Temporary
    I am going to sleep now. See you soon.

--- R/derive_flow.R
+++ R/derive_flow.R
@@ -1,3 +1,11 @@
 derive_flow <- function(){
+  
+  # Check that grass is running
+  running <- check_running()
+  if(!running) stop("No active GRASS session. Program halted.")
+  
+  # Otherwise proceed
+  execGRASS()
+  
   invisible()
 }
