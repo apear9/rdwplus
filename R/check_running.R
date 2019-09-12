473fa1fdd5ea35942e962df0843e24e3ef405676
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-12 22:03:45 +1000

    Export check_running()

--- R/check_running.R
+++ R/check_running.R
@@ -1,3 +1,11 @@
+#' Check whether a valid GRASS session is running
+#' @description This function is mostly used internally by other functions in the package. However, users may call this function to check whether they have correctly established a GRASS session prior to using the other functions in the package.
+#' @return A logical. The logical indicates whether a valid GRASS session is currently running.
+#' @examples 
+#' 
+#' check_running()
+#' 
+#' @export
 check_running <- function(){
   info <- get.GIS_LOCK()
   nchar(info) > 0
