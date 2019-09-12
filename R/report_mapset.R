0d8db001dc4da4ec77609df4ac6df56304fd84e1
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-13 01:41:15 +1000

    Write example for report_mapset()

--- R/report_mapset.R
+++ R/report_mapset.R
@@ -2,6 +2,24 @@
 #' @description GRASS GIS uses a system of mapsets.
 #' @param which One of either 'current' (the default), which causes the function to return the current mapset, or 'possible', which causes the function to list all possible mapsets.
 #' @return Nothing.
+#' @examples 
+#' 
+#' \dontrun{
+#' 
+#' # Get GRASS installation
+#' my_grass <- search_for_grass()[1]
+#' 
+#' # Create GRASS session
+#' initGRASS(my_grass)
+#' 
+#' # Ask for current mapset
+#' report_mapset()
+#' 
+#' # Ask for possible mapsets
+#' report_mapset('possible')
+#' 
+#' }
+#' 
 #' @export
 report_mapset <- function(which = "current"){
   
