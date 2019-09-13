b3f0a8981b2b879b147cc1e3492a55bea417918c
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-13 16:53:58 +1000

    GRASS compliant fill_sinks with new docs

--- R/fill_sinks.R
+++ R/fill_sinks.R
@@ -1,17 +1,52 @@
 #' Fill sinks in a digital elevation model (DEM)
-#' @description A sink is a depression in a DEM. Water flows into these depressions but not out of them. These depressions, although often real features of landscapes, are problematic for flow direction and accumulation algorithms. Therefore, it is common practice to remove these depressions. This function removes depressions (sinks) in a DEM.
-#' @param dem File path to a digital elevation model, in SAGA format (.sgrd extension)
-#' @param out Name of the file to be written out, INCLUDING a .sgrd extension. This will be the filled DEM.
-#' @param ... Optional additional parameters to \code{rsaga.fill.sinks}. Note that the \code{method} is hard-coded to \code{'xxl.wang.liu.2006'}.
-#' @return  Nothing. A file with the name \code{out} will be created in the current working directory.
+#' @description A sink is a depression in a DEM. Water flows into these depressions but not out of them. These depressions, although often real features of landscapes, are problematic for flow direction and accumulation algorithms. Therefore, it is common practice to remove these depressions. This function removes depressions (sinks) in a DEM. Note that this function calls \code{r.hydrodem}, which is a GRASS GIS add-on. It can be installed through the GRASS GUI. 
+#' @param dem The name of a DEM in the current GRASS mapset.
+#' @param out Name of the output, which is a hydrologically corrected (sink-filled) DEM.
+#' @param flags Optional. A vector of flags that should be passed to \code{r.hydrodem}. See details for more on the possible flags.
+#' @param ... Optional additional parameters to \code{r.hydrodem}.
+#' @return  Nothing. A file with the name \code{out} will be created in the current GRASS mapset.
+#' @details 
+#' 
+#' The following flags may be supplied (as strings):
+#' 
+#' \itemize{
+#'     \item "a" The nuclear option. Vigorously remove all sinks.
+#'     \item "overwrite" The output files may overwrite existing ones.
+#'     \item "verbose" Lots of module output.
+#'     \item "quiet" Barely any module output.
+#' }
+#' 
 #' @export
-fill_sinks <- function(dem, out, ...){
+fill_sinks <- function(dem, out, flags, ...){
   
   # Check that input dem file exists
   dem_exist <- file.exists(dem)
   if(!dem_exist) stop(paste0("It appears that no file called ", dem, " exists.")) 
   
+  # Check that GRASS is running
+  if(!check_running()) stop("There is no valid GRASS session. Program halted.")
   
+  # Run r.hydrodem
+  if(missing(flags)){
+    execGRASS(
+      "r.hydrodem",
+      parameters = list(
+        input = dem,
+        output = out,
+        ...
+      )
+    )
+  } else {
+    execGRASS(
+      "r.hydrodem",
+      flags = flags,
+      parameters = list(
+        input = dem,
+        output = out,
+        ...
+      )
+    )
+  }
   
   # Return nothing
   invisible()
