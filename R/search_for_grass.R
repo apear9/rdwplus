4b8072429d1420a38e7930ae03a63782dc5f7cbe
Grace <g.heron@qut.edu.au>
2019-09-13 14:03:40 +1000

    update to search_for_grass
    optional input

--- R/search_for_grass.R
+++ R/search_for_grass.R
@@ -1,13 +1,17 @@
 #' Find GRASS installations
 #' @description This function finds the path to potential GRASS installations. Warning: this function works by brute force, so it may take a few minutes to find potential GRASS installations.
+#' @param guide An optional guide folder to search.
 #' @return A vector of file paths to potential GRASS installations.
 #' @export
-search_for_grass <- function(){
-  
-  if(.Platform$OS.type == 'windows') {
-    potential <- dir("C:/", "GRASS GIS", include.dirs = T, full.names = T, recursive = T)
-  } else {
-    potential <- dir("/usr", "grass", include.dirs = T, full.names = T, recursive = T)
+search_for_grass <- function(guide){
+  if(missing(guide)){
+    if(.Platform$OS.type == 'windows') {
+      potential <- dir("C:/", "GRASS GIS", include.dirs = T, full.names = T, recursive = T)
+    } else {
+      potential <- dir("/usr", "grass", include.dirs = T, full.names = T, recursive = T)
+    }
+  }else{
+    potential <- dir(guide, "GRASS GIS", include.dirs = T, full.names = T, recursive = T)
   }
   dirs <- file.info(potential)$isdir
   potential[dirs]
