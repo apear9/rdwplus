1883f54befedc7037f751792204e55774563edd3
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-13 17:10:28 +1000

    Function to get flow accumulation and direction

--- R/derive_flow.R
+++ R/derive_flow.R
@@ -1,11 +1,41 @@
-derive_flow <- function(){
+#' Obtain flow direction and accumulation over a digital elevation model (DEM)
+#' @description This function computes flow direction and accumulation (among other things) from a DEM. This is done using the \code{r.terraflow} tool in GRASS.
+#' @param dem A digital elevation model that has been hydrologically corrected.
+#' @param flow_dir The name of the output flow direction file in the current GRASS mapset.
+#' @param flow_acc The name of the output flow accumulation file in the current GRASS mapset.
+#' @param flow_tci The name of the output terrain convergence indenx file in the current GRASS mapset.
+#' @param sinks The name of the output sink-watershed file in the current GRASS mapset.
+#' @param overwrite Whether any of the outputs should be allowed to overwrite existing files.
+#' @param ... Additional arguments to \code{r.terraflow}. 
+#' @return Nothing. Files are written in the current GRASS mapset.
+#' @export
+derive_flow <- function(dem, flow_dir, flow_acc, flow_tci, sinks, overwrite = FALSE, ...){
   
   # Check that grass is running
   running <- check_running()
   if(!running) stop("No active GRASS session. Program halted.")
   
-  # Otherwise proceed
-  execGRASS()
+  # Use the following flags
+  flags <- "s"
+  if(overwrite){
+    flags <- c(flags, "overwrite")
+  }
   
+  # Otherwise proceed
+  execGRASS(
+    "r.terraflow",
+    flags = flags,
+    parameters = list(
+      elevation = dem,
+      filled = paste("out", basename(dem), sep = "__"),
+      direction = flow_dir,
+      accumulation = flow_acc,
+      swatershed = sinks,
+      tci = flow_tci,
+      ...
+    )
+  )
+  
+  # Return nothing
   invisible()
 }
