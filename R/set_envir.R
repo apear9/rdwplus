123aff32e0ba9301424b25f3c3fad901f8b419ed
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-14 11:30:21 +1000

    set_envir to allow raster or file path input

--- R/set_envir.R
+++ R/set_envir.R
@@ -1,12 +1,19 @@
 #' Set environment parameters from a GIS layer.
 #' @description This function simplifies the process of setting up a GRASS environment with parameters such as cell snapping, size and mapset extent. 
-#' @param layer File path to a GIS layer that should be used to set environment parameters such as cell size, extent, etc.
-#' @return Nothing.
+#' @param layer A \code{Raster*} object or the file path to a GIS layer that should be used to set environment parameters such as cell size, extent, etc.
+#' @return Nothing. Displays current environment settings.
 #' @export
 set_envir <- function(layer){
+  
+  # Check if GRASS session is running
   if(!check_running()) stop("No active GRASS session. Please use initGRASS.")
 
-  r_layer <- raster(layer)
+  # Check whether input is filepath or is already a raster layer
+  if(!is_raster_layer(layer)){
+    r_layer <- raster(layer)
+  } else {
+    r_layer <- layer
+  }
   
   r_attr <- get_raster_attr(r_layer)
   
