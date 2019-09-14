2195b2f75afa48ea81f875daf5d43365b680806e
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-14 21:35:13 +1000

    Write documentation for snap_sites

--- R/snap_sites.R
+++ R/snap_sites.R
@@ -1,3 +1,12 @@
-snap_sites <- function(){
+#' A function to snap sites in a shapefile to a stream network
+#' @description This function takes a set of survey site locations and makes sure that they are coincident with the stream network dataset.
+#' @param sites File name for a shapefile containing the locations of the survey sites or a \code{SpatialPoints*} object representing the same thing.
+#' @param streams File name for a shapefile containing the edges of the stream network or a \code{SpatialLines*} object representing the same thing.
+#' @param max_move The maximum distance in map units that any site can be moved to snap it to the streams.
+#' @param out The output file path. 
+#' @param overwrite Whether the output should be allowed to overwrite any existing files. Defaults to \code{FALSE}.
+#' @return Nothing. Note that a shapefile of snapped survey sites will be written to the file \code{out} and a shapefile called \code{basename(out)} will be imported into the GRASS mapset.
+#' @export 
+snap_sites <- function(sites, streams, max_move, out, overwrite = FALSE){
   invisible()
 }
