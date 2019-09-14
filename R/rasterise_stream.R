fd77dc9ea0572fdb43116cfb00d4d8d3e01d4a69
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-14 21:16:16 +1000

    Give method a default value, change order of args

--- R/rasterise_stream.R
+++ R/rasterise_stream.R
@@ -1,13 +1,13 @@
 #' Turn a shapefile of stream edges into a raster
 #' @description Given a shapefile of lines representing the channels of a stream network, and a reference raster that defines cell resolution and snapping, this function will return a rasterised version of the shapefile.
 #' @param streams A file name for a shapefile of stream edges or a SpatialLines* object that represents the same thing.
-#' @param reference_raster File path of a raster layer that defines the extent of the study area, as well as the cell resolution and snapping of the output raster. Can also be a \code{Raster*} object. 
+#' @param reference_raster File path of a raster layer that defines the extent of the study area, as well as the cell resolution and snapping of the output raster. Can also be a \code{Raster*} object.
+#' @param out The filename of the output. 
 #' @param method A string. The method that should be used to process the stream. The options are "binary" (stream cells = 1, others = 0) or "unary" (stream cells = NoData, others = 1) 
-#' @param out The filename of the output.
 #' @param overwrite A logical indicating whether the output is allowed to overwrite existing files. Defaults to \code{FALSE}.
 #' @return Nothing. A file will be written to \code{out}. A raster with the name \code{basename(out)} will be written to the current GRASS mapset.
 #' @export
-rasterise_stream <- function(streams, reference_raster, method, out, overwrite = FALSE){
+rasterise_stream <- function(streams, reference_raster, out, method = "binary", overwrite = FALSE){
   
   # Check if a GRASS session exists
   if(!check_running()) stop("There is no valid GRASS session. Program halted.")
