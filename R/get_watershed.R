9e774320badbe907c87a77b1269c1a6844be5d80
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-16 16:18:41 +1000

    Fill out function to retrieve watershed(s)

--- R/get_watershed.R
+++ R/get_watershed.R
@@ -1,12 +1,51 @@
 #' Delineate watershed for a survey site
 #' @description This function delineates a watershed around the \code{i}th site from a set of survey sites.
-#' @param sites A \code{SpatialPoints*} object.
+#' @param sites A file path to a shapefile of points or a \code{SpatialPoints*} object.
 #' @param i An integer which indexes one row of the sites' attribute table.
-#' @param dem Pointer to a hydrologically-corrected digital elevation model.
-#' @param out The name of the output file, with NO file extension.
-#' @param R A logical indicating whether a RasterLayer should be returned in R. Defaults to \code{TRUE}.
-#' @return A SAGA raster with basename \code{out}. Possibly also a \code{RasterLayer} object. 
+#' @param flow_dir The file name of a hydrologically-corrected digital elevation model in the current GRASS mapset.
+#' @param out The name of the output raster. A raster with the name \code{basename(out)} will be imported into the GRASS mapset. If \code{write_file} is true, then a file with the name \code{out} will be written into the user's current working directory.
+#' @param write_file A logical indicating whether the output file should be stored as a file in the user's current working directory. Defaults to \code{FALSE}.
+#' @param overwrite A logical indicating whether the output should be allowed to overwrite existing files. Defaults to \code{FALSE}.
+#' @return Nothing. A raster file with the name \code{out} may be written to the current working directory and one with the name \code{basename(out)} will be imported into the current GRASS mapset. 
 #' @export
-get_watershed <- function(sites, i, dem, out, R = TRUE){
+get_watershed <- function(sites, i, flow_dir, out, write_file = FALSE, overwrite = FALSE){
+  
+  # Check whether GRASS running
+  if(!check_running()) stop("There is no active GRASS session. Program halted.")
+  
+  # Check whether sites is a SpatialPoints
+  if(!is_sppoints(sites)) sites <- shapefile(sites)
+  
+  # Get ith row of coordinate table 
+  cds <- sites@coords[i,1:2] # sometimes the table will have three coordinates?
+
+  # Get x and y
+  x <- cds[1]
+  y <- cds[2]
+  
+  # Define flags
+  flags <- "quiet"
+  if(overwrite) flags <- c(flags, "overwrite")
+  
+  # Execute GRASS command
+  execGRASS(
+    "r.water.outlet",
+    flags = flags,
+    parameters = list(
+      input = flow_dir,
+      output = basename(out),
+      coordinates = c(x, y)
+    )
+  )
+  
+  # Export outside of mapset if requested
+  if(write_file){
+    retrieve_raster(
+      basename(out),
+      out, 
+      overwrite
+    )
+  }
+    
   invisible() 
 }
