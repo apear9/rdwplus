156ab359f448a17023fd51bb1aaa7e8490580cd4
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-19 19:25:53 +1000

    Update docs for compute_metrics

--- R/compute_metrics.R
+++ R/compute_metrics.R
@@ -1,13 +1,13 @@
 #' Compute spatially explicit watershed land use percentages
 #' @description Workhorse function for rdwplus. This function computes the spatially explicit landuse metrics in IDW-PLUS.
 #' @param metrics A character vector. This vector specifies which metric(s) should be calculated. Your options are iFLO, iFLS, iEDO, iEDS, HAiFLO and HAiFLS. The default is to calculate all except for iEDO and iEDS.
-#' @param landuse Names of land use or land cover rasters in the current GRASS mapset for which spatially explicit watershed metrics should be computed.
+#' @param landuse Names of landuse or landcover rasters in the current GRASS mapset for which spatially explicit watershed metrics should be computed.
 #' @param fields A character vector giving the names of the new fields to be created in the sites' attribute table to store the land use metrics. The default is a combination of the metric names and land use raster names. 
-#' @param sites Pointer to a shapefile of sites.
-#' @param elevation A filled (flow-corrected) digital elevation model.
-#' @param flow_dir A 'Deterministic 8' (D8) flow direction grid derived from \code{elevation}.
-#' @param flow_acc A flow accumulation grid derived from \code{flow_dir}.
-#' @param streams A pointer to a streams raster where non-stream cells are coded as 1 and stream cells are coded as NA / NoData. Optional if you are not asking for the iFLS, iEDS, and/or HAiFLS metrics.
+#' @param sites A shapefile of sites; either a file path to the shapefile or a \code{SpatialPoints*} object.
+#' @param elevation File name of a filled (hydrologically-conditioned) digital elevation model in the current GRASS mapset.
+#' @param flow_dir A 'Deterministic 8' (D8) flow direction grid derived from \code{derive_flow}.
+#' @param flow_acc File name of a flow accumulation grid derived from \code{derive_flow} in the current GRASS mapset.
+#' @param streams File name of a streams raster in the current GRASS mapset. Optional if you are not asking for the iFLS, iEDS, and/or HAiFLS metrics.
 #' @param idwp The inverse distance weighting parameter. Default is \code{-1}.
 #' @return A SpatialPointsDataFrame object, which is the \code{sites} argument with a modified attribute table. The table will contain the new land use metrics. 
 #' @export
