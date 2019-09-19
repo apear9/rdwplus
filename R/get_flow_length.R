cfe0a1aff0dee16b4b57b4b240041991913045e2
Grace <g.heron@qut.edu.au>
2019-09-19 12:26:05 +1000

    Complete flow length function
    Filled out skeleton and tested

--- R/get_flow_length.R
+++ R/get_flow_length.R
@@ -1,3 +1,26 @@
-get_flow_length <- function(){
-  invisible()
+#' Derive a flow length to streams and outlets
+#' @description Given a (hydrologically corrected, see \code{\link{fill_sinks}}) DEM, this function produces a flow accumulation grid which shows the upstream area that flows into each cell in the DEM.
+#' @param dem A file path to the hydrologically corrected DEM, with .sgrd extension.
+#' @param out A file name for the output raster of flow lengths.
+#' @param overwrite Overwrite flag. Defaults to \code{FALSE}.
+#' @return Nothing. A file with the name \code{out} will be written to SAGA's current workspace.
+#' @export
+get_flow_length <- function(dem, out, overwrite = FALSE){
+  
+  # Check whether GRASS running
+  if(!check_running()) stop("There is no active GRASS session. Program halted.")
+  
+  # Define flags
+  flags <- "quiet"
+  if(overwrite) flags <- c(flags, "overwrite")
+  
+  execGRASS(
+    "r.flow",
+    flags = flags,
+    parameters = list(
+      elevation = dem,
+      flowlength = out
+    )
+  )
+
 }
