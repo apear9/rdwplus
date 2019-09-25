8cba50bf506d57eff1738d6cb38a078b641ab3b3
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-09-25 17:20:32 +1000

    Function to silence spurious GRASS cmd outputs

---
+++ R/silence.R
@@ -0,0 +1,13 @@
+#' Function to suppress messages, warnings, errors from GRASS commands
+#' @description Prevents the printing GRASS warnings, etc. Use with extreme caution. This is only helpful IF AND ONLY IF you are SURE that any printed messages, warnings, and errors are spurious.
+#' @param value A logical indicating whether GRASS messages, warnings, errors should be suppressed. Can be missing, and it is missing by default.
+#' @return A logical indicating the current status of the option.
+#' @export
+silence <- function(value){
+  
+  # If value is not missing, manipulate option
+  if(!missing(value)) set.ignore.stderrOption(value)
+  # Then simply get the current value
+  get.ignore.stderrOption()
+  
+}
