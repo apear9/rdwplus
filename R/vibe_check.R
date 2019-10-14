e433112e055be622a8e91384bdfb8d0cef163aa4
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-10-14 18:00:19 +1000

    Print --> message

--- R/vibe_check.R
+++ R/vibe_check.R
@@ -7,10 +7,10 @@ vibe_check <- function(){
   # Check if a GRASS session exists
   if(!check_running()) stop("There is no valid GRASS session. Program halted.")
   
-  print("Vectors in mapset:")
+  message("Vectors in mapset:")
   execGRASS("g.list", parameters = list(type = "vector"))
   
-  print("Rasters in mapset:")
+  message("Rasters in mapset:")
   execGRASS("g.list", parameters = list(type = "raster"))
   
   # Return nothing
