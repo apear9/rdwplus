8b31e2df3297307660301c4c4379fad153f4c5d0
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-10-25 12:51:35 +1000

    Clarify defn of binary

--- R/compute_metrics.R
+++ R/compute_metrics.R
@@ -1,7 +1,7 @@
 #' Compute spatially explicit watershed attributes for survey sites on streams
 #' @description Workhorse function for \code{rdwplus}. This function computes the spatially explicit landuse metrics in IDW-Plus (Peterson and Pearse, 2017).
 #' @param metrics A character vector. This vector specifies which metric(s) should be calculated. Your options are lumped, iFLO, iFLS, iEDO, iEDS, HAiFLO and HAiFLS. The default is to calculate all except for lumped, iEDO and iEDS.
-#' @param landuse Names of landuse or landcover binary rasters in the current GRASS mapset for which spatially explicit watershed metrics should be computed.
+#' @param landuse Names of binary landuse or landcover rasters in the current GRASS mapset for which spatially explicit watershed metrics should be computed. Binary means land use cells are coded 1 and all other cells are given a value of 0.
 #' @param sites A shapefile of sites; either a file path to the shapefile or a \code{SpatialPoints*} object.
 #' @param elevation File name of a filled (hydrologically-conditioned) digital elevation model in the current GRASS mapset.
 #' @param flow_dir A 'Deterministic 8' (D8) flow direction grid derived from \code{derive_flow}.
