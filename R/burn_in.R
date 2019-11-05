99361472542f6a6983a2317b815ed3a06e786908
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-11-05 16:38:55 +1000

    Update burn-in, vignette still not building

--- R/burn_in.R
+++ R/burn_in.R
@@ -2,7 +2,7 @@
 #' @description Burning-in streams (also called 'drainage reinforcement') ensures flow direction and accumulation grids based on the digital elevation model will correctly identify the stream path.
 #' @param dem Digital elevation model raster in the GRASS mapset. 
 #' @param stream Binary stream raster in the GRASS mapset. 
-#' @param out Name of output to be crated in the GRASS mapset. 
+#' @param out Name of output to be created in the GRASS mapset. 
 #' @param burn The magnitude of the drainage reinforcement in elevation units. Defaults to \code{10} elevation units.
 #' @param overwrite A logical indicating whether the file \code{out} should be overwritten in the mapset and on disk. Defaults to \code{FALSE}.
 #' @return Nothing. A raster with the name \code{out} will be written to the current GRASS mapset.
@@ -24,27 +24,6 @@ burn_in <- function(dem, stream, out, burn = 10, overwrite = FALSE){
     flags = flags,
     parameters = list(expression = exec_cmd)
   )
-  
-  # If dem and stream are file paths, then import them as rasters
-  # if(is.character(dem)) dem <- raster(dem)
-  # if(is.character(stream)) stream <- raster(stream)
-  
-  # Input checking
-  # check_dem <- is_raster_layer(dem)
-  # check_stm <- is_raster_layer(stream)   
-  # if(!check_dem) stop("dem is not a Raster* object")
-  # if(!check_stm) stop("stream is not a Raster* object")
-  
-  # More input checking
-  # check_stm <- is_binary_raster(stream)
-  # if(!check_stm) stop("stream is not a binary raster.")
-  
-  # Burn in the stream
-  # burned <- dem - burn * stream
-  
-  # Write raster to file
-  # writeRaster(burned, out, overwrite = overwrite)
-  # raster_to_mapset(out, overwrite = overwrite, max_memory = max_memory)
 
   # Return nothing
   invisible()
---
+++ vignettes/using_rdwplus.R
@@ -0,0 +1,58 @@
+## ----setup, include=FALSE------------------------------------------------
+knitr::opts_chunk$set(
+  echo = TRUE,
+  eval = TRUE, # switch to TRUE when compiling final documentation
+  message=FALSE,
+  warning=FALSE
+)
+
+## ------------------------------------------------------------------------
+library(rdwplus)
+
+## ------------------------------------------------------------------------
+dem <- system.file("extdata", "dem.tif", package = "rdwplus")
+lus <- system.file("extdata", "landuse.tif", package = "rdwplus")
+sts <- system.file("extdata", "site.shp", package = "rdwplus")
+stm <- system.file("extdata", "streams.shp", package = "rdwplus")
+
+## ------------------------------------------------------------------------
+# Path to GRASS
+my_grass <- "C:/Program Files/GRASS GIS 7.6"
+# If you don't know where the GRASS installation sits on your
+# computer, use the following.
+# Note this may yield more than one directory, hence the [1]
+# my_grass <- search_for_grass()[1]
+
+## ------------------------------------------------------------------------
+initGRASS(my_grass, mapset = "PERMANENT", override = TRUE)
+
+## ------------------------------------------------------------------------
+check_running()
+
+## ------------------------------------------------------------------------
+# Give this function a filepath for a raster
+set_envir(dem)
+
+## ------------------------------------------------------------------------
+raster_to_mapset(c(dem, lus), as_integer = c(FALSE, TRUE), overwrite = TRUE)
+
+## ------------------------------------------------------------------------
+vector_to_mapset(c(sts, stm), overwrite = TRUE)
+
+## ------------------------------------------------------------------------
+rasterise_stream("streams", "streams.tif", TRUE)
+reclassify_streams("streams.tif", "streams01.tif", "binary", overwrite = TRUE)
+
+
+## ------------------------------------------------------------------------
+# Drainage reinforcement
+burn_in(dem, "streams01.tif", "burndem.tif", overwrite = TRUE)
+
+## ------------------------------------------------------------------------
+# Fill dem
+fill_sinks("burndem.tif", "filldem.tif", flags = "a", overwrite = TRUE)
+
+## ------------------------------------------------------------------------
+# Derive flow direction and accumulation rasters
+derive_flow("filldem.tif", "flowdir.tif", "flowacc.tif", overwrite = TRUE)
+
