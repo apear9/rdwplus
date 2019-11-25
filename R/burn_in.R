3bc14eaf485fc4eacdb069f73c9fb6700c988749
Grace <g.heron@qut.edu.au>
2019-11-25 10:26:25 +1000

    burn_in example

--- R/burn_in.R
+++ R/burn_in.R
@@ -6,6 +6,30 @@
 #' @param burn The magnitude of the drainage reinforcement in elevation units. Defaults to \code{10} elevation units.
 #' @param overwrite A logical indicating whether the file \code{out} should be overwritten in the mapset and on disk. Defaults to \code{FALSE}.
 #' @return Nothing. A raster with the name \code{out} will be written to the current GRASS mapset.
+#' @examples 
+#' if(!check_running()){
+#' ## Initialise session
+#' my_grass <- search_for_grass()[1]
+#' initGRASS(gisBase = my_grass, override = TRUE, mapset = "PERMANENT")
+#' 
+#' ## Load data set
+#' dem <- system.file("extdata", "dem.tif", package = "rdwplus")
+#' stream_shp <- system.file("extdata", "streams.shp", package = "rdwplus")
+#' set_envir(dem)
+#' raster_to_mapset(dem)
+#' vector_to_mapset(stream_shp)
+#' 
+#' ## Create binary stream
+#' rasterise_stream("streams", "streams_rast")
+#' reclassify_streams("streams_rast", "streams_binary", out_type = "binary")
+#' 
+#' ## Burn dem 
+#' burn_in(dem = "dem.tif", stream = "streams_binary", out = "dem_burn", burn = 10, overwrite = FALSE)
+#' 
+#' ## Plot
+#' plot_GRASS("dem_burn", col = topo.colors(10))
+#' 
+#' }
 #' @export 
 burn_in <- function(dem, stream, out, burn = 10, overwrite = FALSE){
   
@@ -24,7 +48,7 @@ burn_in <- function(dem, stream, out, burn = 10, overwrite = FALSE){
     flags = flags,
     parameters = list(expression = exec_cmd)
   )
-
+  
   # Return nothing
   invisible()
   
