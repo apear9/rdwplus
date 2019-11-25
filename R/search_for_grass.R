62ac80b2f6e1406b172fc74f17b3938c9e0de34a
Grace <g.heron@qut.edu.au>
2019-11-25 14:06:43 +1000

    search_for_grass example

--- R/search_for_grass.R
+++ R/search_for_grass.R
@@ -2,6 +2,11 @@
 #' @description This function finds the path to potential GRASS installations. Warning: this function works by brute force, so it may take a few minutes to find potential GRASS installations.
 #' @param guide An optional guide folder to search.
 #' @return A vector of file paths to potential GRASS installations.
+#' @examples 
+#' \donttest{
+#' my_grass <- search_for_grass()
+#' my_grass
+#' }
 #' @export
 search_for_grass <- function(guide){
   if(missing(guide)){
