21397fe061c1a12eb9b76752377ff874c8769169
Alan R. Pearse <17267197+apear9@users.noreply.github.com>
2019-11-29 13:11:00 +1000

    Add to search_for_grass docs

--- R/search_for_grass.R
+++ R/search_for_grass.R
@@ -1,15 +1,16 @@
 #' Find GRASS installations
-#' @description This function finds the path to potential GRASS installations. 
+#' @description This function finds the path to potential GRASS installations. It does so in a very crude way; that is, by searching for directories that match the string \code{'GRASS'}.  
 #' 
 #' Warning: this function works by brute force, so it may take a few minutes to find potential GRASS installations.
 #' 
-#' Note: it is not hard to find the path to your computer's GRASS installation yourself. This is the preferred course of action.
+#' Note: This is not guaranteed to work. It is not hard to find the path to your computer's GRASS installation yourself. This is the preferred course of action.
 #'  
 #' @param guide Optional. A specific folder to search in for the GRASS installation.
 #' @return A vector of file paths to potential GRASS installations.
 #' @examples 
 #' \dontrun{ 
 #' 
+#' # Not run because this takes a few minutes
 #' my_grass <- search_for_grass()
 #' my_grass
 #' 
