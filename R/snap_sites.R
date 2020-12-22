9f87799a4986e256eb524f980263655a5080615a
Grace <g.heron@qut.edu.au>
2020-12-22 15:22:23 +1000

    option to chose flow_acc or streams for snapping sites

--- R/snap_sites.R
+++ R/snap_sites.R
@@ -1,14 +1,16 @@
 #' A function to snap sites in a shapefile to a stream grid
 #' @description This function takes a set of survey site locations and makes sure that they are coincident with the point nearest the stream line within a specified distance. This is equivalent to snapping sites to a stream network. Note that this function calls \code{r.stream.snap}, which is a GRASS GIS add-on. It can be installed through the GRASS GUI.
+#'
 #' @param sites File name for a shapefile containing the locations of the survey sites in the current GRASS mapset.
-#' @param streams File name for a binary streams raster in the current GRASS mapset. Snaps points to closest point on stream line.
+#' @param streams File name for a binary streams raster in the current GRASS mapset. Snaps points to closest point on stream line. Defaults to NULL. 
+#' @param flow_acc File name for a flow accumulation raster in the current GRASS mapset. Flow accumulation will snap to largest accumulation point. Defaults to NULL. 
 #' @param max_move The maximum distance in cells that any site can be moved to snap it to the flow accumulation grid.
 #' @param out The output file path. 
 #' @param overwrite Whether the output should be allowed to overwrite any existing files. Defaults to \code{FALSE}.
 #' @param max_memory Max memory used in memory swap mode (MB). Defaults to \code{300}.
 #' @param use_sp Logical to use 'sf' or 'stars' classes for vector objects. Defaults to \code{TRUE} to use 'stars' class.
 #' @param ... Additional arguments to \code{r.stream.snap}.
-#'
+#' 
 #' @return Nothing. Note that a shapefile of snapped survey sites will be written to the location \code{out} and a shapefile called \code{basename(out)} will be imported into the GRASS mapset.
 #' @examples 
 #' # Will only run if GRASS is running
@@ -36,7 +38,7 @@
 #' 
 #' }
 #' @export 
-snap_sites <- function(sites, streams, max_move, out, overwrite = FALSE, max_memory = 300, use_sp = TRUE, ...){
+snap_sites <- function(sites, streams = NULL, flow_acc = NULL, max_move, out, overwrite = FALSE, max_memory = 300, use_sp = TRUE, ...){
   
   # Check if a GRASS session exists
   if(!check_running()) stop("There is no valid GRASS session. Program halted.")
@@ -50,18 +52,32 @@ snap_sites <- function(sites, streams, max_move, out, overwrite = FALSE, max_mem
   grass_out <- basename(out)
   grass_out <- gsub(".shp", "", grass_out)
   
-  execGRASS(
-    "r.stream.snap",
-    flags = flags,
-    parameters = list(
-      input = sites,
-      stream_rast = streams, 
-      output = grass_out,
-      radius = max_move,
-      memory = max_memory,
-      ...
-    )
-  )
+  if(!is.null(streams) & is.null(flow_acc)){
+    execGRASS(
+      "r.stream.snap",
+      flags = flags,
+      parameters = list(
+        input = sites,
+        stream_rast = streams, 
+        output = grass_out,
+        radius = max_move,
+        memory = max_memory,
+        ...
+      )
+    )} else if(is.null(streams) & !is.null(flow_acc)){
+      execGRASS(
+        "r.stream.snap",
+        flags = flags,
+        parameters = list(
+          input = sites,
+          accumulation = flow_acc, 
+          output = grass_out,
+          radius = max_move,
+          memory = max_memory,
+          ...
+        ))} else {
+          stop("There are no streams or flow accumulation")
+        }
   
   # Read in sites and grass out as vector points
   sites <- readVECT(sites)
