bcd9408bca66eeac7da1cb38096b19512ab4caa8
Alan <17267197+apear9@users.noreply.github.com>
2021-11-10 10:57:14 +1000

    Will probs change to r.fill.dir
    Need to do more testing in grass before i commit to this

--- R/fill_sinks.R
+++ R/fill_sinks.R
@@ -1,5 +1,5 @@
 #' Fill sinks in a digital elevation model (DEM)
-#' @description A sink is a depression in a DEM. Water flows into these depressions but not out of them. These depressions, although often real features of landscapes, are problematic for flow direction and accumulation algorithms. Therefore, it is common practice to remove these depressions. This function removes depressions (sinks) in a DEM. Note that this function calls \code{r.hydrodem}, which is a GRASS GIS add-on. It can be installed through the GRASS GUI. 
+#' @description A sink is a depression in a DEM. Water flows into these depressions but does not flow out of them. These depressions, although often real features of landscapes, are problematic for flow direction and accumulation algorithms. Therefore, it is common practice to remove these depressions. This function removes depressions (sinks) in a DEM. Note that this function calls \code{r.hydrodem}, which is a GRASS GIS add-on. It can be installed through the GRASS GUI. 
 #' @param dem The name of a DEM in the current GRASS mapset.
 #' @param out Name of the output, which is a hydrologically corrected (sink-filled) DEM.
 #' @param flags Optional. A vector of flags that should be passed to \code{r.hydrodem}. See details for more on the possible flags.
