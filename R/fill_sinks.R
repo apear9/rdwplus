929f391599549affdd423941d1afd8ea155711f5
Alan <17267197+apear9@users.noreply.github.com>
2021-11-27 17:39:09 +1000

    Fix error in fill_sinks

--- R/fill_sinks.R
+++ R/fill_sinks.R
@@ -1,7 +1,8 @@
 #' Fill sinks in a digital elevation model (DEM)
 #' @description A sink is a depression in a DEM. Water flows into these depressions but does not flow out of them. These depressions, although often real features of landscapes, are problematic for flow direction and accumulation algorithms. Therefore, it is common practice to remove these depressions. This function removes depressions (sinks) in a DEM. Note that this function calls \code{r.hydrodem}, which is a GRASS GIS add-on. It can be installed through the GRASS GUI. 
 #' @param dem The name of a DEM in the current GRASS mapset.
-#' @param out Name of the output, which is a hydrologically corrected (sink-filled) DEM.
+#' @param out_dem Name of the output DEM, which is a hydrologically corrected (sink-filled) DEM.
+#' @param out_fd Name of the output flow direction map for the sink-filled DEM.
 #' @param overwrite A logical indicating whether the output should be allowed to overwrite existing files. Defaults to \code{FALSE}.
 #' @param ... Optional additional parameters to \code{r.fill.dir}.
 #' @return  Nothing. A file with the name \code{out} will be created in the current GRASS mapset.
@@ -11,32 +12,25 @@
 #' 
 #' }
 #' @export
-fill_sinks <- function(dem, out, overwrite = FALSE, ...){
+fill_sinks <- function(dem, out_dem, out_fd, overwrite = FALSE, ...){
   
   # Check that GRASS is running
   if(!check_running()) stop("There is no valid GRASS session. Program halted.")
   
   # Run r.fill.dir
-  if(overwrite){
-    execGRASS(
-      "r.fill.dir",
-      flags = "overwrite",
-      parameters = list(
-        input = dem,
-        output = out,
-        ...
-      )
+  flags <- NULL
+  if(overwrite) flags <- "overwrite"
+  
+  execGRASS(
+    "r.fill.dir",
+    flags = flags,
+    parameters = list(
+      input = dem,
+      output = out_dem,
+      direction = out_fd,
+      ...
     )
-  } else {
-    execGRASS(
-      "r.fill.dir",
-      parameters = list(
-        input = dem,
-        output = out,
-        ...
-      )
-    )
-  }
+  )
   
   # Return nothing
   invisible()
