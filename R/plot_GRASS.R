301806a0b90a4f5c94205f37806c81ae00a690ec
Alan <17267197+apear9@users.noreply.github.com>
2021-11-30 16:19:36 +1000

    pivot plot_GRASS to stars style

--- R/plot_GRASS.R
+++ R/plot_GRASS.R
@@ -1,43 +1,12 @@
 #' A function to plot a raster from the current GRASS mapset
-#' @description Given the name of a raster in the current GRASS mapset, this function will plot it as a \code{RasterLayer} object.
+#' @description Given the name of a raster in the current GRASS mapset, this function will plot it as a \code{stars} object.
 #' @param x The name of an object in the current GRASS mapset.
 #' @param out_x Optional. If supplied, the function makes a call to \code{\link{retrieve_raster}} and writes out the raster to the file path \code{out_x}. Otherwise the function will write the layer to \code{tempdir}.
-#' @param ... Additional arguments to \code{plot}.
+#' @param colours Optional. A colour scale. If not supplied, the default settings in \code{plot.stars} is used. If supplied, \code{length(colours)} must be less than or equal to the number of unique values in the raster.
+#' @param ... Additional arguments to \code{plot.stars}.
 #' @return Nothing.
-#' @examples
-#' # Will only run if GRASS is running
-#' if(check_running()){
-#' 
-#' # Load data set
-#' dem <- system.file("extdata", "dem.tif", package = "rdwplus")
-#' landuse <- system.file("extdata", "landuse.tif", package = "rdwplus")
-#' sites <- system.file("extdata", "site.shp", package = "rdwplus")
-#' stream_shp <- system.file("extdata", "streams.shp", package = "rdwplus")
-#' 
-#' # Set environment parameters and import data to GRASS
-#' set_envir(dem)
-#' raster_to_mapset(rasters = c(dem, landuse), as_integer = c(FALSE, TRUE))
-#' vector_to_mapset(vectors = c(sites, stream_shp))
-#' 
-#' # Rasterise streams and sites
-#' rasterise_stream("streams", "streams_rast.tif", overwrite = TRUE)
-#' point_to_raster(outlets = "site", out = "sites_rast.tif", overwrite = TRUE)
-#' 
-#' # Plot
-#' # Set number 1
-#' par(mfrow = c(1,2))
-#' plot_GRASS("dem.tif", col = topo.colors(15))
-#' plot_GRASS("sites_rast.tif", col = heat.colors(1), add = TRUE)
-#' 
-#' # Set number 2
-#' plot_GRASS("landuse.tif", col = topo.colors(2))
-#' plot_GRASS("streams_rast.tif", col = heat.colors(1), add = TRUE)
-#' 
-#' # Reset plotting device parameters
-#' par(mfrow = c(1, 1))
-#' }
 #' @export
-plot_GRASS <- function(x, out_x, ...){
+plot_GRASS <- function(x, out_x, colours, ...){
   
   # Check if GRASS is running
   if(!check_running()) stop("There is currently no valid GRASS session. Program halted.")
@@ -51,9 +20,16 @@ plot_GRASS <- function(x, out_x, ...){
   }
   
   # Load into r
-  r <- raster(out_x)
-
+  r <- read_stars(out_x)
+  
+  # Compute the number of breaks
+  computed_breaks <- length(colours) + 1
+  
   # Display raster
-  plot(r, ...)
+  if(missing(colours)){
+    plot(r) 
+  } else {
+    plot(r, col = colours, nbreaks = computed_breaks, ...)
+  }
   
 }
