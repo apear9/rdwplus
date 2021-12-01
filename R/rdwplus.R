95701a9717361845eaa3db3652dd4735663079f2
Alan <17267197+apear9@users.noreply.github.com>
2021-12-01 13:31:19 +1000

    Trying to solve warning for imported S3 method plot.stars

--- R/rdwplus.R
+++ R/rdwplus.R
@@ -4,7 +4,7 @@
 #' 
 #' @docType package
 #' @name rdwplus
-#' @importFrom stars read_stars plot
+#' @import stars 
 #' @import rgrass7
 #' @importFrom methods is
 #' @importFrom utils read.csv write.csv
