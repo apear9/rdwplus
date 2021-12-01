2b849fd15efc786818252c913203f6377b786145
Alan <17267197+apear9@users.noreply.github.com>
2021-12-01 11:22:18 +1000

    switch order of args

--- R/plot_GRASS.R
+++ R/plot_GRASS.R
@@ -1,12 +1,12 @@
 #' A function to plot a raster from the current GRASS mapset
 #' @description Given the name of a raster in the current GRASS mapset, this function will plot it as a \code{stars} object.
 #' @param x The name of an object in the current GRASS mapset.
-#' @param out_x Optional. If supplied, the function makes a call to \code{\link{retrieve_raster}} and writes out the raster to the file path \code{out_x}. Otherwise the function will write the layer to \code{tempdir}.
 #' @param colours Optional. A colour scale. If not supplied, the default settings in \code{plot.stars} is used. If supplied, \code{length(colours)} must be less than or equal to the number of unique values in the raster.
+#' @param out_x Optional. If supplied, the function makes a call to \code{\link{retrieve_raster}} and writes out the raster to the file path \code{out_x}. Otherwise the function will write the layer to \code{tempdir}.
 #' @param ... Additional arguments to \code{plot.stars}.
 #' @return Nothing.
 #' @export
-plot_GRASS <- function(x, out_x, colours, ...){
+plot_GRASS <- function(x, colours, out_x, ...){
   
   # Check if GRASS is running
   if(!check_running()) stop("There is currently no valid GRASS session. Program halted.")
