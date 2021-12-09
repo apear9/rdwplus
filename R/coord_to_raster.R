002fbbdaed0bc8f16efe5a2137ed3e302ea90a4a
Alan <17267197+apear9@users.noreply.github.com>
2021-12-10 00:02:45 +1000

    major rework of this helper function
    way simpler than before

--- R/coord_to_raster.R
+++ R/coord_to_raster.R
@@ -1,6 +1,7 @@
 #' Turn coordinates of outlets into rasters
 #' @description Given a set of coordinates in space (x, y), this function will return a rasterised version of that point in space.
-#' @param outlet A single pair of Easting, Northing or long, lat coordinates as a numeric vector. 
+#' @param outlets The name of a set of sites in the current GRASS mapset.
+#' @param which The site to convert to raster.
 #' @param out The file name of the output outlet raster in the current GRASS mapset.
 #' @param overwrite Whether the output files should be allowed to overwrite existing files. Defaults to \code{FALSE}.
 #' @return Nothing.
@@ -27,7 +28,7 @@
 #' plot_GRASS("coords", col = "red", add = TRUE)
 #' }
 #' @export
-coord_to_raster <- function(outlet, out, overwrite = FALSE){
+coord_to_raster <- function(outlets, which, out, overwrite = FALSE){
   
   # Check grass running
   if(!check_running()) stop("There is no valid GRASS session. Program halted.")
@@ -36,36 +37,15 @@ coord_to_raster <- function(outlet, out, overwrite = FALSE){
   flags <- c("quiet")
   if(overwrite) flags <- c(flags, "overwrite")
   
-  # Convert coords to text
-  text <- data.frame(x = outlet[1], y = outlet[2])
-  where_temp <- paste0(tempdir(), "/temp_hold_coord.csv")
-  write.csv(text, where_temp, row.names = FALSE)
-  
-  # Convert coords to point
-  point <- paste0("point_", basename(out))
-  execGRASS(
-    "v.in.ascii",
-    flags = c(flags, "n"),
-    parameters = list(
-      input = where_temp,
-      output = point,
-      separator = ",",
-      format = "point",
-      skip = 1
-    )
-  )
-  
-  # Clean up temporary file
-  file.remove(where_temp)
-  
   # Convert point to raster
   execGRASS(
     "v.to.rast",
     flags = flags,
     parameters = list(
-      input = point,
+      input = outlets,
       output = basename(out),
-      use = "cat"
+      use = "cat",
+      cat = as.character(which)
     )
   )
   
