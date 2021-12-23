a756af3531315461d1270c77e8c3cff7ad518729
Alan <17267197+apear9@users.noreply.github.com>
2021-12-23 15:20:45 +1000

    Update zonal_table.R

--- R/zonal_table.R
+++ R/zonal_table.R
@@ -1,13 +1,18 @@
 zonal_table <- function(inmap, inlu, outtab){
+  
+  # Set flags
   flags <- c("overwrite",  "t", "g", "quiet")
   
-  execGRASS("r.univar",
-            flags = flags,
-            parameters = list(
-              map = inmap, 
-              zones = inlu, 
-              output = outtab, 
-              separator = "comma"
-            )
+  # Create Zonal table
+  execGRASS(
+    "r.univar",
+    flags = flags,
+    parameters = list(
+      map = inmap, 
+      zones = inlu, 
+      output = outtab, 
+      separator = "comma"
+    )
   )
+  
 }
