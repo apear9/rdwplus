64d4fc05f4eaba39d6f3012d831d51cadb4c167a
Alan <17267197+apear9@users.noreply.github.com>
2022-04-15 15:44:13 +1000

    Fixed small mistake

--- R/zeros_to_nodata.R
+++ R/zeros_to_nodata.R
@@ -1,5 +1,5 @@
 zeros_to_nodata <- function(x, o, overwrite = FALSE){
-  expr_calc <- paste0(o, " = if(x, x, null())")
+  expr_calc <- paste0(o, " = if(", x, ",", x, ", null())")
   flags <- "quiet"
   if(overwrite) flags <- c(flags, "overwrite")
   execGRASS(
