ef22629133a15b0654fb6b37fd7a68ca052eeb01
Alan <17267197+apear9@users.noreply.github.com>
2022-04-19 17:45:28 +1000

    Add watershed mask

--- R/compute_iFLO_weights.R
+++ R/compute_iFLO_weights.R
@@ -1,6 +1,7 @@
 #' Compute iFLO weights
 #' @description Compute an iFLO weight raster outside of the \code{compute_metrics()} function.
-#' @param pp Pour point raster containing a single pour point (i.e., the outlet).
+#' @param pour_point Pour point raster containing a single pour point (i.e., the outlet).
+#' @param watershed Watershed raster to use as a mask for the flow-path calculations.
 #' @param null_streams A streams raster with NoData for the stream cells and 1s everywhere else
 #' @param fd A flow direction raster.
 #' @param out_flow_length Name of the output flow length raster.
@@ -10,18 +11,16 @@
 #' @param remove_streams A logical indicating whether cells corresponding to the stream line should be removed from the weights raster. Defaults to \code{FALSE}.
 #' @return Nothing.
 #' @export
-compute_iFLO_weights <- function(pp, null_streams, fd, out_flow_length, out_no_stream, out_iFLO, idwp = -1, remove_streams = FALSE, ...){
-  # pp - pour point as rasterised x-y coordinate
-  # null_streams - streams raster with NoData for the stream cells and 1s everywhere else
-  # fd - flow direction grid
-  # idwp - inverse distance weighting power
-  # remove streams - whether stream cells should be removed
-  # out_flow_length - name of distance raster
-  # out_no_stream - name of distance raster without outlet, redundant if remove_streams is FALSE
-  # out_iFLO - name of output weights raster
+compute_iFLO_weights <- function(pour_point, watershed, null_streams, fd, out_flow_length, out_no_stream, out_iFLO, idwp = -1, remove_streams = FALSE, ...){
+
+  # Set mask
+  set_mask(watershed)
   
+  # Clear mask on exit
+  on.exit(clear_mask())
+    
   # Flow lengths
-  get_flow_length(str_rast = pp, flow_dir = flow_dir, out = out_flow_length, to_outlet = TRUE, ...)
+  get_flow_length(str_rast = pour_point, flow_dir = flow_dir, out = out_flow_length, to_outlet = TRUE, ...)
   
   # Remove outlet cell if necessary
   if(remove_streams){
