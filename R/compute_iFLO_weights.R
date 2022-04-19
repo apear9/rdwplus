3066c9cc8ffbc3e211ac09f0c12b320b2ef7da5b
Alan <17267197+apear9@users.noreply.github.com>
2022-04-20 09:01:24 +1000

    Argument name fix

--- R/compute_iFLO_weights.R
+++ R/compute_iFLO_weights.R
@@ -3,7 +3,7 @@
 #' @param pour_point Pour point raster containing a single pour point (i.e., the outlet).
 #' @param watershed Watershed raster to use as a mask for the flow-path calculations.
 #' @param null_streams A streams raster with NoData for the stream cells and 1s everywhere else
-#' @param fd A flow direction raster.
+#' @param flow_dir A flow direction raster.
 #' @param out_flow_length Name of the output flow length raster.
 #' @param out_iFLO Name of the output weights raster.
 #' @param out_iFLO_no_stream Name of the output weights raster excluding cells on the stream line (ignored inf \code{remove_streams = FALSE}). 
@@ -11,7 +11,7 @@
 #' @param remove_streams A logical indicating whether cells corresponding to the stream line should be removed from the weights raster. Defaults to \code{FALSE}.
 #' @return Nothing.
 #' @export
-compute_iFLO_weights <- function(pour_point, watershed, null_streams, fd, out_flow_length, out_iFLO, out_iFLO_no_stream, idwp = -1, remove_streams = FALSE, ...){
+compute_iFLO_weights <- function(pour_point, watershed, null_streams, flow_dir, out_flow_length, out_iFLO, out_iFLO_no_stream, idwp = -1, remove_streams = FALSE, ...){
 
   # Set mask
   set_mask(watershed)
