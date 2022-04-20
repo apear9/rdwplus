0e72690a95858ca9586a36781dbb7491f038609f
Alan <17267197+apear9@users.noreply.github.com>
2022-04-20 10:33:05 +1000

    Expose rast_calc to user

--- R/rast_calc.R
+++ R/rast_calc.R
@@ -1,3 +1,8 @@
+#' Raster calculator (wrapper for "r.mapcalc")
+#' @param ex A valid raster calculator expression for GRASS.
+#' @param overwrite Defaults to \code{TRUE}.
+#' @return Nothing.
+#' @export
 rast_calc <- function(ex, overwrite = TRUE){
   
   if(!check_running()) stop("There is no valid GRASS session. Program halted.")
